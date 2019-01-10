1bba97b0c7a0d1839bbb4b46d342b718018b23a1
Aakash Hemadri <aakashhemadri123@gmail.com>
2019-01-10 20:26:53 +0530

    CRC fix.

--- crc/crc.py
+++ crc/crc.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Wed Dec 19 11:06:54 2018
@@ -15,12 +16,12 @@ def crc():
     """Cyclic Redundancy Check"""
     divisor = bitarray(endian='big')
     divisor.extend([1,0,1,1,1])
-    orignal_dataword = ask()
-    dataword = orignal_dataword
+    original_dataword = ask()
+    dataword = original_dataword
     dataword.extend([i^i for i in range(divisor.count())])
     remainder = dataword
     remainder = remainder[:divisor.length()]
-    for i in range(divisor.length(),dataword.length())
+    for i in range(divisor.length(),dataword.length()):
         if remainder[0] == True:
             remainder = remainder ^ divisor
         else:
@@ -29,6 +30,7 @@ def crc():
         if i < dataword.length():
             remainder.append(dataword[i])
     original_dataword.extend(remainder)
+    return original_dataword
     
 def test():
     """Test function"""
